71c2e30c0c604e2b74d2f9c18ee9e3400c414565
dmnbc <31969318+dmnbc@users.noreply.github.com>
2023-01-04 18:25:12 +0100

    php entfernt

--- index.php
+++ index.php
@@ -14,23 +14,10 @@
    
 </head>
 	
-	
-	
 <body>
 	 <img id="logo" src="logo.svg" alt="Logo" width="150" height="90" >
 	
-	<?php
-     $ip = $_SERVER["REMOTE_ADDR"];  
-      $host = gethostbyaddr($ip);
-	$t=time();
-      $myfile = fopen("visitors.txt", "a") or die("Unable to open file!");
-		$txt = $ip."\t".$host."\t".date("Y-m-d H:i",$t);
-		fwrite($myfile, "\n". $txt);
-		fclose($myfile);
-      echo "IP Adresse: $ip<br>";  
-      echo "Hostname: $host";  
 
-   ?>
 	<p> <a href="https://dmnb.consulting/impressum.html">Impressum</a> </p>
 </body>
 </html>
