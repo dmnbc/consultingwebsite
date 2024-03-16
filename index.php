d31d6c68a6c35541449d0408d394beefcfcfa843
dmnbc-praktikant <83332455+dmnbc-praktikant@users.noreply.github.com>
2024-03-16 17:32:03 +0100

    first uploads from apatana test

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="de">
+<head>
+    <meta charset="utf-8">
+    <title>dmnbc consulting start static page on aws amplify</title>
+	<style>
+		#logo{ float:right;}
+		p{ clear:right;	}
+	</style>
+   
+</head>
+	
+<body>
+
+	 <?php echo "Hello" ?>
+
+	 <img id="logo" src="logo.svg" alt="Logo" width="150" height="90" >
+	<h3>Diese Testseite für AWS Kurs</h3>
+        Auf dieser Seite zeigt AWS Ihnen anschaulich die Möglichkeiten eines AWS Amplify Services mit einer github Integration.<br><br>Es werden dabei keine personenbezogenen Daten erfasst.
+	<p> <a href="https://dmnb.consulting/Products.html">Produkte</a> </p>
+	<p> <a href="https://dmnb.consulting/impressum.html">Impressum</a> </p>
+	<p> <a href="https://dmnb.consulting/datenschutz.html">Datenschutzhinweise</a> </p>
+</body>
+</html>
